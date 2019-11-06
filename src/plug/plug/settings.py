35ed6a84f2d91271c2ba95a78bde298d473ed012
Kamil <kamil.daniewski@gmail.com>
2019-11-06 02:04:43 +0100

    Added allowed host

--- src/plug/plug/settings.py
+++ src/plug/plug/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["68.183.208.207:8000"]
 
 
 # Application definition
